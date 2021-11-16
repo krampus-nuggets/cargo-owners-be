af76f7ebe189ee4fd8aef58a4e3d5b40ab9b94e8
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-11-16 12:17:59 +0200

    add: checkConnection helper

--- src/modules/database.js
+++ src/modules/database.js
@@ -9,3 +9,18 @@ const collections = {
     transporter: "transporter-rates",
     average: "average-rates"
 }
+
+async function checkConnection() {
+    let checkState = false;
+
+    try {
+        await client.connect();
+        await client.db(process.env.DB_NAME).command({ ping: 1 });
+        checkState = true;
+    } catch (err) {
+        console.error();
+    } finally {
+        await client.close();
+        return checkState;
+    }
+}
