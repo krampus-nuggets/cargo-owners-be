fe5526a7a1d303996da2762bfe95691470502dc9
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-11-16 17:42:54 +0200

    update: add CORS headers

--- index.js
+++ index.js
@@ -48,6 +48,18 @@ app.use(function (req, res, next) {
     next();
 });
 
+
+// Apply CORS Headers
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 // FUNC: Authenticate The Session
 function authenticate(user, pass) {
     let userCheck = false;
