0a2a9941610a3d8c7f23041c8dd61319afe85392
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-11-16 12:17:40 +0200

    add: collections object

--- src/modules/database.js
+++ src/modules/database.js
@@ -4,3 +4,8 @@ const { MongoClient } = require("mongodb");
 dotenv.config();
 const connectURI = process.env.DB_URL;
 const client = new MongoClient(connectURI);
+
+const collections = {
+    transporter: "transporter-rates",
+    average: "average-rates"
+}
